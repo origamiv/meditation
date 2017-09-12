70436f884ff0bc0a4769b3f09fb73cf4b60341bb
origamiv2 <origamiv@gmail.com>
2017-09-12 11:53:27 +0300

    1

---
+++ zip.bat
@@ -0,0 +1,19 @@
+@ECHO Zipping
+    del /Q project.zip
+    rmdir testproject /s /q
+ 
+    mkdir testproject
+    mkdir testproject/hooks
+    copy config.xml testproject\config.xml
+    copy .DS_Store "testproject/.DS_Store"
+    copy icon.png "testproject/icon.png"
+    copy package.json "testproject/package.json"
+
+    xcopy /y /e hooks testproject\hooks\
+    xcopy /y /e platforms testproject\platforms\
+    xcopy /y /e www testproject\www\
+    xcopy /y /e plugins testproject\plugins\
+
+"c:\Program Files\7-Zip\7z.exe" a project.zip testproject
+c:\curl\bin\curl -u origamiv@gmail.com:9030404 -X PUT -F file=@project.zip http://build.phonegap.com/api/v1/apps/2563816
+c:\curl\bin\curl -u origamiv@gmail.com:9030404 -X POST -d '' https://build.phonegap.com/api/v1/apps/2563816/build
